8a11a4ccca583b243c1c10c610bb65a73df5af64
prakritisubedii <prakriti.subedi@bison.howard.edu>
2025-01-24 08:30:22 -0500

    hello world

--- LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,8 +1,14 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
+	
+	/**
+	 * Prakriti Subedi
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
+		System.out.println("Hello LSP!!!!!!");
 		// TODO Auto-generated method stub
 
 	}
